2d2fd93c49ab7ea0cc7bb27005cab9ee9e8f42dc
maxim_t <maxim_t@klever.dp.ua>
2020-12-26 23:02:13 +0200

    Task 1.6

---
+++ app/code/Training/TestOM/Model/PlayWithTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 26.12.2020
+ * Time: 22:56
+ */
+
+namespace Training\TestOM\Model;
+class PlayWithTest
+{
+    private $testObject;
+    private $testObjectFactory;
+    private $manager;
+
+    public function __construct(
+        \Training\TestOM\Model\Test $testObject,
+        \Training\TestOM\Model\TestFactory $testObjectFactory,
+        \Training\TestOM\Model\ManagerCustomImplementation $manager
+    )
+    {
+        $this->testObject = $testObject;
+        $this->testObjectFactory = $testObjectFactory;
+        $this->manager = $manager;
+    }
+
+    public function run()
+    {
+// test object with constructor arguments managed by di.xml
+        $this->testObject->log();
+// test object with custom constructor arguments
+// some arguments are defined here, others - from di.xml
+        $customArrayList = ['item1' => 'aaaaa', 'item2' => 'bbbbb'];
+        $newTestObject = $this->testObjectFactory->create([
+            'arrayList' => $customArrayList,
+            'manager' => $this->manager
+        ]);
+        $newTestObject->log();
+    }
+}
